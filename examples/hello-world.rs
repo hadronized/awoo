8433a9717d65224dcd3b8971c5ca6341a27acf67
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-07-19 16:26:42 +0200

    Fully implement an example!

--- examples/hello-world.rs
+++ examples/hello-world.rs
@@ -1,9 +1,16 @@
+use awoo::scheduler::RandomAccessScheduler;
+use awoo::time::simple::SimpleF32TimeGenerator;
 use awoo::window::Window;
 
 fn main() {
-  let a = Window::new(0., 3.);
-  let b = Window::new(3., 10.);
+  let a = Window::new(0., 3.).map(|t| println!("in a: {}", t));
+  let b = Window::new(3., 10.).map(|t| println!("in b: {}", t));
 
-  println!("{}:{}", a.start, a.end);
-  println!("{}:{}", b.start, b.end);
+  let mut scheduler =
+    RandomAccessScheduler::new(
+      SimpleF32TimeGenerator::new(0., 1.),
+      vec![a, b]
+    ).expect("cannot create scheduler");
+
+  scheduler.schedule();
 }
